56dee5973d0b25e6c6b6decf1b715244c9deba6d
Yasmineltair <yasmineltair293@gmail.com>
2023-11-16 18:27:01 -0800

    up

--- pint_func.c
+++ pint_func.c
@@ -8,10 +8,12 @@
 
 void pint_func(stack_t **head, unsigned int line_number)
 {
+	FILE *file;
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		free_stack(*head);
+		fclose(file);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*head)->n);
