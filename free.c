8c6b91a22cac6b0a804e60efa07bfee3971eeab6
Yasmineltair <yasmineltair293@gmail.com>
2023-11-16 17:20:22 -0800

    up

---
+++ free.c
@@ -0,0 +1,62 @@
+#include "monty.h"
+
+/**
+  * free_stack - free linked list
+  * @head: header
+ */
+
+void free_stack(stack_t *head)
+{
+	stack_t *temp;
+
+	temp = head;
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp
+	}
+}
+
+/**
+  * ex - execute opcode
+  * @line: line content
+  * @stack: stack linked list
+  * @line_number: line_number
+  * @file: pointer to monty file
+  * Return: 0 / 1
+  */
+
+int ex(char *line, stack_t **stack, unsigned int line_number, FILE *file)
+{
+	instruction_t op_list[] = {
+		{"push", push_func},
+		{"pall", pall_func},
+		{NULL, NULL}
+	};
+	unsigned int i = 0;
+	char *opcode;
+
+	opcode = strtok(line, " \n\t");
+	if (opcode && opcode[0] == '#')
+		return (0);
+	value = strtok(NULL, " \n\t");
+	while (op_list[i].opcode && opcode)
+	{
+		if (strcmp(opcode, op_list[i].opcode) == 0)
+		{
+			op_list[i].f(stack, line_number);
+			return (0);
+		}
+		i++;
+	}
+	if (opcode && op_list[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+		fclose(file);
+		free(line);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	return (1);
+}
---
+++ main.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+  * main - entry point
+  * @argc: number of args
+  * @argv: value of args
+  * Return: 0 on success
+  */
+
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	ssize_t r_line = 1;
+	char *value;
+	size_t length = 0;
+	unsigned int line_number;
+	stack_t *stack = NULL;
+
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (r_line > 0)
+	{
+		value = NULL;
+		r_line = getline(&line, &length, file);
+		line_number++;
+		if (r_line > 0)
+			ex(line, &stack, line_number, file)
+		free(line);
+	}
+	free_stack(stack);
+	fclose(file);
+	return (0);
+}
+
